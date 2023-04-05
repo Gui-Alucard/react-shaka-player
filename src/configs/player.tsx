5f8481418c5146bb913cd07e3ede1705776745eb
g-silva-oliveira <g.silva.oliveira@accenture.com>
2023-04-05 11:39:32 -0300

    :fire: Super Configs
    Removo as keys não reconhecidas pelo shaka (player, ui)

--- src/configs/player.tsx
+++ src/configs/player.tsx
@@ -1,75 +1,69 @@
 const vodConfig = {
-  ui: {
-    addSeekBar: true,
-    doubleClickForFullscreen: true,
-    singleClickForPlayAndPause: true,
-    enableKeyboardPlaybackControls: true,
-    enableFullscreenOnRotation: true,
-    forceLandscapeOnFullscreen: true,
-    clearBufferOnQualityChange: true,
-    addBigPlayButton: false,
-    customContextMenu: false,
-    controlPanelElements: [
-      'play_pause',
-      'mute',
-      'volume',
-      'time_and_duration',
-      'spacer',
-      'fullscreen',
-      'quality',
-      'picture_in_picture'
-    ],
-    seekBarColors: {
-      base: '#ffffff4c',
-      buffered: '#ffffff4c',
-      played: '#e93035',
-      adBreaks: '#ffcc00',
-    },
-    volumeBarColors: {
-      base: '#ffffff4c',
-      level: '#0060ad'
-    }
+  addSeekBar: true,
+  doubleClickForFullscreen: true,
+  singleClickForPlayAndPause: true,
+  enableKeyboardPlaybackControls: true,
+  enableFullscreenOnRotation: true,
+  forceLandscapeOnFullscreen: true,
+  clearBufferOnQualityChange: true,
+  addBigPlayButton: false,
+  customContextMenu: false,
+  controlPanelElements: [
+    'play_pause',
+    'mute',
+    'volume',
+    'time_and_duration',
+    'spacer',
+    'fullscreen',
+    'quality',
+    'picture_in_picture'
+  ],
+  volumeBarColors: {
+    base: '#ffffff4c',
+    level: '#0060ad'
   },
+  seekBarColors: {
+    base: '#ffffff4c',
+    buffered: '#ffffff4c',
+    played: '#e93035',
+    adBreaks: '#ffcc00',
+  }
 };
 
 const streamingConfig = {
-  player: {
-    manifest: {
-      dash: {
-        ignoreMinBufferTime: true,
-      },
-    },
-    streaming: {
-      lowLatencyMode: true,
-      inaccurateManifestTolerance: 0,
-      rebufferingGoal: 1,
-      smallGapLimit: 1,
-      jumpLargeGaps: true,
-      durationBackoff: 0
+  manifest: {
+    dash: {
+      ignoreMinBufferTime: true,
     },
   },
-  ui: {
-    addSeekBar: true,
-    doubleClickForFullscreen: true,
-    singleClickForPlayAndPause: true,
-    enableKeyboardPlaybackControls: true,
-    enableFullscreenOnRotation: true,
-    forceLandscapeOnFullscreen: true,
-    clearBufferOnQualityChange: false,
-    addBigPlayButton: false,
-    customContextMenu: false,
-    controlPanelElements: ['play_pause', 'mute', 'volume', 'spacer', 'fullscreen', 'quality'],
-    seekBarColors: {
-      base: '#ffffff4c',
-      buffered: '#ffffff4c',
-      played: '#e93035',
-      adBreaks: '#ffcc00',
-    },
-    volumeBarColors: {
-      base: '#ffffff4c',
-      level: '#0060ad'
-    }
+  streaming: {
+    lowLatencyMode: true,
+    inaccurateManifestTolerance: 0,
+    rebufferingGoal: 1,
+    smallGapLimit: 1,
+    jumpLargeGaps: true,
+    durationBackoff: 0
   },
+  addSeekBar: true,
+  doubleClickForFullscreen: true,
+  singleClickForPlayAndPause: true,
+  enableKeyboardPlaybackControls: true,
+  enableFullscreenOnRotation: true,
+  forceLandscapeOnFullscreen: true,
+  clearBufferOnQualityChange: false,
+  addBigPlayButton: false,
+  customContextMenu: false,
+  controlPanelElements: ['play_pause', 'mute', 'volume', 'spacer', 'fullscreen', 'quality'],
+  volumeBarColors: {
+    base: '#ffffff4c',
+    level: '#0060ad'
+  },
+  seekBarColors: {
+    base: '#ffffff4c',
+    buffered: '#ffffff4c',
+    played: '#e93035',
+    adBreaks: '#ffcc00',
+  }
 };
 
 export { vodConfig, streamingConfig };
