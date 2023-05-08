1692dc7a861d2b6dd6aff5ebcb13f8274979bdef
g-silva-oliveira <g.silva.oliveira@accenture.com>
2023-05-08 16:19:55 -0300

    :adhesive_bandage: Button
    Retiro a function que estava disparando

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,39 +5,23 @@ import { Unmute, Rewind, Foward } from '../icons';
 const Button = (props: IPlayerProps) => {
   const { onClick, label } = props;
 
-  const handleIcon = (label: string) => {
-    if (label && label === 'unmute') {
-      return (
-        <div id={label} className={`sbt-theme-container-${label}`} >
-          <button onClick={onClick}>
-            <Unmute width={20} height={18.87} style={{ marginRight: '5px' }} /> <p>Ativar som</p>
-          </button>
-        </div>
-      )
-    } else if (label && label === 'foward') {
-      return (
-        <div id={label} className={`sbt-theme-container-${label}`} >
-          <button onClick={onClick}>
-            <Foward width={36} height={42} /> <p>Avançar</p>
-          </button>
-        </div>
-      )
-    } else if (label && label === 'rewind') {
-      return (
-        <div id={label} className={`sbt-theme-container-${label}`} >
-          <button onClick={onClick}>
-            <Rewind width={36} height={41} /> <p>Retroceder</p>
-          </button>
-        </div>
-      )
-    }
-  }
-
   return (
-    <div id={label} className={`sbt-theme-container-${label}`} >
-      <button onClick={onClick}>
-        {label && handleIcon(label)}
-      </button>
+    <div className='sbt-theme-button-container' >
+      {label && label === 'unmute' && (
+        <button className='unmute' onClick={onClick}>
+          <Unmute width={20} height={18.87} style={{ marginRight: '5px' }} /> <p>Ativar som</p>
+        </button>
+      )}
+      {label && label === 'foward' && (
+        <button className='foward' onClick={onClick}>
+          <Foward width={36} height={42} /> <p>Avançar</p>
+        </button>
+      )}
+      {label && label === 'rewind' && (
+        <button className='rewind' onClick={onClick}>
+          <Rewind width={36} height={41} /> <p>Retroceder</p>
+        </button>
+      )}
     </div>
   )
 };
