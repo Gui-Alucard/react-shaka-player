ff5f9494ad46963f9539daa4a54000925b9c1f64
g-silva-oliveira <g.silva.oliveira@accenture.com>
2023-05-16 11:00:35 -0300

    :rocket: Rewind
    Coloco consoles para teste

--- src/components/rewind.tsx
+++ src/components/rewind.tsx
@@ -4,19 +4,21 @@ import { IPlayerProps } from '../types';
 const RewindButton = (props: IPlayerProps) => {
   const { onClick, label } = props;
 
-  const handleEnter = () => {
+  const handleEnter = (e: any) => {
+    console.log('[ Rewind OVER', e)
     const element = document.getElementsByClassName('shaka-controls-container');
     element.item(0).setAttribute('shown', 'true');
   }
 
-  const handleLeave = () => {
+  const handleLeave = (e: any) => {
+    console.log('[ Rewind LEAVE', e)
     const element = document.getElementsByClassName('shaka-controls-container');
     element.item(0).removeAttribute('shown');
   }
 
   return (
     <div className='sbt-theme-rewind-container'>
-      <button type='button' onClick={onClick} onMouseEnter={handleEnter} onMouseLeave={handleLeave}><p>{label && label}</p></button>
+      <button type='button' onClick={onClick} onMouseOver={(e) => handleEnter(e)} onMouseLeave={(e) => handleLeave(e)}><p>{label && label}</p></button>
     </div>
   )
 };
