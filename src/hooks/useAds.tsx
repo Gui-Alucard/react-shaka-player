399909ecf480522462b815c3c78c3db903c9d31d
g-silva-oliveira <g.silva.oliveira@accenture.com>
2023-06-07 10:26:27 -0300

    :zap: Tooltips e Ads
    removo tooltips e adiciono os eventos para os Ads

--- src/hooks/useAds.tsx
+++ src/hooks/useAds.tsx
@@ -4,32 +4,55 @@ import { useEffect } from "react";
 import { IPlayerProps } from "../types";
 
 const useAds = (ui: ShakaUI.Overlay, player: ShakaPlayer, props?: IPlayerProps) => {
-
   useEffect(() => {
     if (player && props.adsTagUrl && props.adsRequest && ui) {
       const adManager = player.getAdManager();
       const mediaElement = player.getMediaElement();
       const container = ui.getControls().getClientSideAdContainer();
+      const stats = adManager.getStats();
 
       const ADS_REQUEST = props.adsRequest;
       const TAG_URL = props.adsTagUrl;
-      ADS_REQUEST.setContinuousPlayback(true);
-      ADS_REQUEST.setAdWillAutoPlay(true);
-      ADS_REQUEST.setAdWillPlayMuted(true);
-      ADS_REQUEST.linearAdSlotHeight = 100;
-      ADS_REQUEST.linearAdSlotWidth = 100;
-      ADS_REQUEST.nonLinearAdSlotHeight = 100;
-      ADS_REQUEST.nonLinearAdSlotWidth = 100;
-      ADS_REQUEST.adTagUrl = TAG_URL;
 
       adManager.initClientSide(container, mediaElement);
 
+      const _handleWindowMessages = (eventName: string) => {
+        // @ts-ignore
+        window.postMessage(JSON.stringify({ event: eventName, data: { adManager: stats, player: player.getStats() } }));
+      };
+
       const _streamRequest = async () => {
+        ADS_REQUEST.setContinuousPlayback(true);
+        ADS_REQUEST.setAdWillAutoPlay(true);
+        ADS_REQUEST.setAdWillPlayMuted(true);
+        ADS_REQUEST.linearAdSlotHeight = 100;
+        ADS_REQUEST.linearAdSlotWidth = 100;
+        ADS_REQUEST.nonLinearAdSlotHeight = 100;
+        ADS_REQUEST.nonLinearAdSlotWidth = 100;
+        ADS_REQUEST.adTagUrl = TAG_URL;
         try {
           // @ts-ignore
           await adManager.requestClientSideAds(ADS_REQUEST);
           adManager.addEventListener(ShakaAds.AdManager.ADS_LOADED, () => {
             mediaElement.play();
+          });
+          adManager.addEventListener(ShakaAds.AdManager.AD_STARTED, () => {
+            _handleWindowMessages('AD_STARTED');
+          });
+          adManager.addEventListener(ShakaAds.AdManager.AD_PROGRESS, () => {
+            _handleWindowMessages('AD_PROGRESS');
+          });
+          adManager.addEventListener(ShakaAds.AdManager.AD_FIRST_QUARTILE, () => {
+            _handleWindowMessages('AD_FIRST_QUARTILE');
+          });
+          adManager.addEventListener(ShakaAds.AdManager.AD_MIDPOINT, () => {
+            _handleWindowMessages('AD_MIDPOINT');
+          });
+          adManager.addEventListener(ShakaAds.AdManager.AD_THIRD_QUARTILE, () => {
+            _handleWindowMessages('AD_THIRD_QUARTILE');
+          });
+          adManager.addEventListener(ShakaAds.AdManager.AD_COMPLETE, () => {
+            _handleWindowMessages('AD_COMPLETE');
           });
         } catch (error) {
           props.onPlayerError && props.onPlayerError(error);
@@ -39,7 +62,7 @@ const useAds = (ui: ShakaUI.Overlay, player: ShakaPlayer, props?: IPlayerProps) 
       _streamRequest();
     }
 
-  }, [props.adsTagUrl, ui]);
+  }, [props.adsRequest, props.adsTagUrl, ui]);
 };
 
 export default useAds;
